cae633ea0bbe2202db2163e2da776533838dd27d
System Administrator <root@Jodies-Macbook.local>
2018-12-18 21:43:48 -0500

    Auto-generated commit

--- src/app/models/restaurant.modle.ts
+++
@@ -1,8 +0,0 @@
-export default class Restaurant {
-    constructor (public id: number,
-        public name: string,
-        public location: string,
-        public address: string,
-        public review: string,
-        public imgpath: string) {}
-}
